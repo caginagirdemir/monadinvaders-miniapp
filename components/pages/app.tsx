249f36cf619600716a86afa8d34890b77441e832
Cagin Agirdemir <caginagirdemir@gmail.com>
2025-05-08 20:33:55 +0000

    wallet actions

--- components/pages/app.tsx
+++ components/pages/app.tsx
@@ -14,7 +14,6 @@ export default function Home() {
   return (
     <SafeAreaContainer insets={context?.client.safeAreaInsets}>
        <IframeGame />
-       <WalletActions />
     </SafeAreaContainer>
   );
 }
