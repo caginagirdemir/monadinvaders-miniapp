79d003be828303972055c879740be7e977ee0dc3
Cagin Agirdemir <caginagirdemir@gmail.com>
2025-05-17 21:00:01 +0000

    layout changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,18 +7,18 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Monad Farcaster MiniApp Template",
-  description: "A template for building mini-apps on Farcaster and Monad",
+  title: "Monad Invaders",
+  description: "Monad Invaders",
   other: {
     "fc:frame": JSON.stringify({
       version: "next",
-      imageUrl: "https://monadinvaders-miniapp.vercel.app/feed.png",
+      imageUrl: "https://monadinvaders-miniapp-dev.vercel.app/feed.png",
       button: {
-        title: "Oyna",
+        title: "Play Game dev",
         action: {
           type: "launch_frame",
           name: "Monad Invaders",
-          url: "https://monadinvaders-miniapp.vercel.app",
+          url: "https://monadinvaders-miniapp-dev.vercel.app",
           splashImageUrl: "https://monadinvaders-miniapp.vercel.app/images/splash.png",
           splashBackgroundColor: "#000000"
         }
