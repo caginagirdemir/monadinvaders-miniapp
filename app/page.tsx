8df2bd88b7779338f8531fd66b06952e00830573
Cagin Agirdemir <caginagirdemir@gmail.com>
2025-05-18 07:09:51 +0000

    debug3

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,4 @@
-import { Metadata } from "next";
 import App from "@/components/pages/app";
-import { APP_URL } from "@/lib/constants";
-
-const frame = {
-  version: "next",
-  imageUrl: `${APP_URL}/images/feed.png`,
-  button: {
-    title: "Play Game 123",
-    action: {
-      type: "launch_frame",
-      name: "Monad Invaders",
-      url: 'https://monadinvaders-miniapp.vercel.app',
-      splashImageUrl: `${APP_URL}/images/splash.png`,
-      splashBackgroundColor: "#ffffff",
-    },
-  },
-};
-
 
 export default function Home() {
   return <App />;
