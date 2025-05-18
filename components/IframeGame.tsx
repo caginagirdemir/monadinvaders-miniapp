935cc98d657530f47b377479c2238ae5920d8f99
Cagin Agirdemir <caginagirdemir@gmail.com>
2025-05-18 06:41:11 +0000

    test2

--- components/IframeGame.tsx
+++ components/IframeGame.tsx
@@ -1,19 +1,36 @@
-import React from "react";
+import Head from "next/head";
 
 export default function IframeGame() {
   return (
-    <iframe
-      src="/game/index.html"
-      sandbox="allow-scripts allow-same-origin allow-modals"
-      style={{
-        width: "100%",
-        aspectRatio: "3 / 2",
-        border: "none",
-        backgroundColor: "black",
-        display: "block"
-      }}
-      
-      title="Game"
-    />
+    <>
+      <Head>
+        <meta name="fc:frame" content='{
+          "version": "vNext",
+          "imageUrl": "https://monadinvaders-miniapp-dev.vercel.app/images/feed.png",
+          "buttons": [
+            {
+              "title": "Play Game",
+              "action": {
+                "type": "link",
+                "url": "https://monadinvaders.xyz"
+              }
+            }
+          ]
+        }' />
+      </Head>
+
+      <iframe
+        src="/game/index.html"
+        sandbox="allow-scripts allow-same-origin allow-modals"
+        style={{
+          width: "100%",
+          aspectRatio: "3 / 2",
+          border: "none",
+          backgroundColor: "black",
+          display: "block"
+        }}
+        title="Game"
+      />
+    </>
   );
 }
