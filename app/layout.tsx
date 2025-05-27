42c47891c74edf40f6d3ec10e2d39fb84e56a4b6
Cagin Agirdemir <caginagirdemir@gmail.com>
2025-05-27 19:35:19 +0000

    meta test

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <title>Monad Invaders</title>
+      <meta name="description" content="Monad Invaders" />
+
+      <meta
+        property="fc:frame"
+        content='{
+          "version": "next",
+          "imageUrl": "https://monadinvaders-miniapp.vercel.app/images/feed.png",
+          "iconUrl": "https://monadinvaders-miniapp.vercel.app/images/splash.png",
+          "name": "Monad Invaders",
+          "button": {
+            "title": "Play Game",
+            "action": {
+              "type": "launch_frame",
+              "name": "Monad Invaders",
+              "url": "https://monadinvaders-miniapp.vercel.app",
+              "splashImageUrl": "https://monadinvaders-miniapp.vercel.app/images/splash.png",
+              "splashBackgroundColor": "#000000"
+            }
+         />
       <body className={inter.className}>
         <FrameProvider>{children}</FrameProvider>
       </body>
